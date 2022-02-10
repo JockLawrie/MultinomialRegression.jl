056bf4873e8383f529ab45ddb582de03e3899d8d
jocklawrie <jock.lawrie@gmail.com>
2022-02-10 18:41:55 +1100

    Restructured diagnostics code to be less cumbersome.

--- src/diagnostics.jl
+++ src/diagnostics.jl
@@ -3,11 +3,18 @@ Support functions for constructing and displaying model diagnostics.
 """
 module diagnostics
 
-export _isregularized, _nparams, _coef, _vcov, _stderror
+export _isregularized, _nparams, _coef, _vcov, _stderror,  # Supporting functions to be used in type-specific code
+       coef, vcov, stderror, nobs, nparams, loglikelihood, isregularized,  # Functions to be overloaded in type-specific code
+       aic, aicc, bic, coefcor, coeftable  # Derived diagnostics
 
 using AxisArrays
+using Distributions
 using PrettyTables
+using StatsBase
 import Base: show  # Overload for 1D and 2D AxisArrays
+
+################################################################################
+# Supporting functions to be used in type-specific code
 
 _isregularized(loglikelihood, loss) = loss != -loglikelihood  # loss == -LL + penalty
 
@@ -56,7 +63,63 @@ function _vcov(vcov, xnames, ynames)
 end
 
 ################################################################################
-# Functions for displaying a fitted model
+# Functions to be overloaded in type-specific code
+function coef end
+function vcov end
+function stderror end
+function nobs end
+function nparams end
+function loglikelihood end
+function isregularized end
+
+################################################################################
+# Derived diagnostics
+function aic(m)
+    isregularized(m) && @warn "Model is regularized. AIC is not based on MLE."
+    2.0 * (nparams(m) - loglikelihood(m))
+end
+
+function aicc(m)
+    LL = loglikelihood(m)
+    k  = nparams(m)
+    n  = nobs(m)
+    isregularized(m) && @warn "Model is regularized. AICc is not based on MLE."
+    2.0*(k - LL) + 2.0*k*(k - 1.0)/(n - k - 1.0)
+end
+
+function bic(m)
+    isregularized(m) && @warn "Model is regularized. BIC is not based on MLE."
+    -2.0*loglikelihood(m) + nparams(m)*log(nobs(m))
+end
+
+function coefcor(m)
+    v        = vcov(m)
+    ni       = size(v, 1)
+    se       = [sqrt(abs(v[i,i])) for i = 1:ni]
+    data     = StatsBase.cov2cor(v, se)
+    colnames = collect(v.axes[2].val)
+    rownames = collect(v.axes[1].val)
+    AxisArray(data, rownames=rownames, colnames=colnames)
+end
+
+function coeftable(m, ci_level::Float64=0.95)
+    b        = coef(m)
+    v        = vcov(m)
+    levstr   = isinteger(ci_level*100) ? string(Integer(ci_level*100)) : string(ci_level*100)
+    ni       = length(b)
+    bvec     = reshape(b, ni)
+    se       = [sqrt(abs(v[i,i])) for i = 1:ni]
+    z        = bvec ./ se
+    pvals    = 2 * ccdf.(Ref(Normal()), abs.(z))
+    ci_width = -se * quantile(Normal(), (1-ci_level)/2)
+    data     = hcat(bvec, se, z, pvals, bvec-ci_width, bvec+ci_width)
+    colnames = ["Coef", "StdError", "z", "Pr(>|z|)", "Lower $levstr%", "Upper $levstr%"]
+    rownames = collect(v.axes[1].val)
+    AxisArray(data, rownames=rownames, colnames=colnames)
+end
+
+################################################################################
+# Unexported functions that support the display of diagnostics
 
 function construct_vcov_rownames(xnames, ynames)
     yname_maxlen  = maximum(length.(ynames)) + 4  # +2 for "y=_  "
--- src/diagnostics_multinomial.jl
+++ src/diagnostics_multinomial.jl
@@ -2,7 +2,7 @@
 Implementation of the diagnostics API for instances of MultinomialRegressionModel.
 
 These functions are defined here:
-- coef(m), vcov(m), nobs(m), nparams(m), loglikelihood(m), isregularized(m)
+- coef(m), vcov(m), stderror(m), nobs(m), nparams(m), loglikelihood(m), isregularized(m)
 
 These functions are automatically derived:
 - aic(m), aicc(m), bic(m), coefcor(m), coeftable(m, confidence_level)
@@ -11,12 +11,11 @@ module diagnostics_multinomial
 
 export isregularized, nobs, loglikelihood,  # Model diagnostics
        nparams, coef, stderror, vcov,       # Coefficient diagnostics
-       aic, aicc, bic, coefcor, coeftable   # Diagnostics from other diagnostics
+       aic, aicc, bic, coefcor, coeftable   # Diagnostics derived from other diagnostics
 
-using ..diagnostics
 import ..fitpredict: MultinomialRegressionModel
-
-include("deriveddiagnostics.jl")
+using ..diagnostics
+import ..diagnostics: coef, vcov, stderror, nobs, nparams, loglikelihood, isregularized  # To be overloaded
 
 # Model-level diagnostics (aic, aicc, bic, coefcor, coeftable are defined in the deriveddiagnostics module)
 isregularized(m::MultinomialRegressionModel) = _isregularized(m.loglikelihood, m.loss)
